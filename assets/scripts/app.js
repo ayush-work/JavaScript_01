b96b9f11282003bf22013c2e0898e08d0619dbf5
ayush2431 <ayushmaurya.work@gmail.com>
2021-01-10 18:38:49 +0530

    day2 lec 39

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,3 +1,22 @@
-let currentResult = 0;
-currentResult += 10;
-outputResult(currentResult, "Hey");
+const defaultValue = 0;
+let currentResult = defaultValue;
+
+function addValue() {
+  const enteredValue = parseInt(userInput.value);
+  const calcLog = `${currentResult}+ ${enteredValue}`;
+  currentResult = currentResult + enteredValue;
+  outputResult(currentResult, calcLog);
+}
+
+function subValue() {
+  const enteredValue = parseInt(userInput.value);
+  const calcLog = `${currentResult}- ${enteredValue}`;
+  currentResult = currentResult - enteredValue;
+  outputResult(currentResult, calcLog);
+}
+
+function mulValue() {}
+function divValue() {}
+
+addBtn.addEventListener("click", addValue);
+subtractBtn.addEventListener("click", subValue);
